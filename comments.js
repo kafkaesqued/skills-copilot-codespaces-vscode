9c2053570ef4ab6a88f27e15dc3442f78ed91b17
kafkaesqued <166281521+kafkaesqued@users.noreply.github.com>
2025-03-03 17:36:18 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,28 +1,30 @@
-//create a web server
-const http = require('http');
-const fs = require('fs');
-const port = 3000;
+// Create web server
+// 1. Create a new Express app
+// 2. Create a new route that responds to a POST request to /comments
+// 3. Parse the form data
+// 4. Add the comment to the comments array
+// 5. Redirect the user back to the home page
+// 6. Create a new route that responds to a GET request to /comments
+// 7. Respond with a JSON representation of the comments array
 
-const requestHandler = (request, response) => {
-    fs.readFile('index.html', (error, data) => {
-        if (error) {
-            response.writeHead(404, {'Content-Type': 'text/html'});
-            response.write('File not found');
-            response.end();
-        } else {
-            response.writeHead(200, {'Content-Type': 'text/html'});
-            response.write(data);
-            response.end();
-        }
-    });
-}
+var express = require('express');
+var bodyParser = require('body-parser');
 
-const server = http.createServer(requestHandler);
+var app = express();
+var comments = [];
 
-server.listen(port, (error) => {
-    if (error) {
-        console.log('Something went wrong', error);
-    } else {
-        console.log('Server is listening on port ' + port);
-    }
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/comments', function(req, res) {
+  var comment = req.body.comment;
+  comments.push(comment);
+  res.redirect('/');
+});
+
+app.get('/comments', function(req, res) {
+  res.json(comments);
+});
+
+app.listen(3000, function() {
+  console.log('Server is listening on port 3000');
 });
